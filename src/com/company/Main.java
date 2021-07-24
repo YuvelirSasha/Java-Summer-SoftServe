a4c8c1f3dc209fd25538106f5e7c7a25a18fa2b0
YuvelirSasha <kripaknomer1@gmail.com>
2021-07-24 21:11:12 +0300

    add lesson 5 and homework 2 in main place

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,4 +1,5 @@
 package com.company;
+import java.io.Console;
 import java.util.Scanner;
 
 public class Main {
@@ -161,9 +162,211 @@ public class Main {
                 System.out.println("Money Bob will be paid: " + (linecode_case3 * 50 / 100 - timesArrivedLate_case3));
         }
     }
+    public static void Lesson5() {
+        //task 1
+        System.out.print("Enter number: ");
+        int displayNumber = number.nextInt();
+        for(int i =0; i <= displayNumber; i++){
+            System.out.println(i);
+        }
+
+        //task 2
+        System.out.print("Enter first number: ");
+        int firstNumber = number.nextInt();
+        System.out.print("Enter second number: ");
+        int secondNumber = number.nextInt();
+
+        if (firstNumber > secondNumber) {
+            for (int i = secondNumber; secondNumber <= firstNumber; secondNumber++) {
+                System.out.println(secondNumber);
+                if (secondNumber % 2 == 0) {
+                    System.out.println(secondNumber);
+                }
+            }
+        }
+        else
+        {
+            for (int i = firstNumber; firstNumber <= secondNumber; firstNumber++) {
+                System.out.println(firstNumber);
+                if (firstNumber % 2 != 0) {
+                    System.out.println(firstNumber);
+                }
+
+                if(firstNumber % 7 ==0)
+                    System.out.println(firstNumber);
+            }
+        }
+
+        //task 3
+
+        System.out.print("Enter first number: ");
+        int firstNumber_2 = number.nextInt();
+        System.out.print("Enter second number: ");
+        int secondNumber_2 = number.nextInt();
+        int sum = 0;
+        for(int i = secondNumber_2; secondNumber_2 <= firstNumber_2; secondNumber_2++)
+        {
+            sum += secondNumber_2;
+            System.out.println(sum);
+        }
+
+        //task 4
+        int count = 0;
+        int nUMBER = 1;
+        while (nUMBER != 0) {
+            System.out.print("Enter numbers: ");
+            nUMBER = number.nextInt();
+            count++;
+            System.out.println(count);
+        }
+
+        //task 5
+
+        System.out.print("Enter first side rectangle: ");
+        int sidefirst = number.nextInt();
+
+        System.out.print("Enter second side rectangle: ");
+        int sidesecond = number.nextInt();
+
+        System.out.print("Enter made of the given symbol: ");
+        String symbol = number.next();
+
+        for(int i = 1; i <= sidefirst; i++)
+        {
+            for (int j = 1; j < sidesecond; j++)
+                System.out.print(symbol);
+            System.out.println(symbol);
+
+        }
+
+        //task 6
+
+        final String ANSI_RED = "\u001B[31m";
+        final String ANSI_BLUE = "\u001B[34m";
+
+        System.out.print("Enter first side rectangle: ");
+        int sidefirst_2 = number.nextInt();
+
+        System.out.print("Enter second side rectangle: ");
+        int sidesecond_2 = number.nextInt();
+
+        System.out.print("Enter made of the given symbol: ");
+        String symbol_2 = number.next();
+
+        System.out.print("Enter the color of the rectangle: ");
+        String color = number.next();
+
+        switch (color) {
+            case "blue":
+                for (int i = 0; i < sidefirst_2; i++) {
+                    for (int j = 0; j < sidesecond_2; j++) {
+                        if (i == 0 || j == 0 || i == sidefirst_2 - 1 || j == sidesecond_2 - 1)
+                            System.out.print(symbol_2 + ANSI_BLUE);
+                        else
+                            System.out.print(" ");
+                    }
+                    System.out.println(" ");
+                }
+                break;
+
+            case "red":
+                for (int i = 0; i < sidefirst_2; i++) {
+                    for (int j = 0; j < sidesecond_2; j++) {
+                        if (i == 0 || j == 0 || i == sidefirst_2 - 1 || j == sidesecond_2 - 1)
+                            System.out.print(symbol_2 + ANSI_RED);
+                        else
+                            System.out.print(" ");
+                    }
+                    System.out.println(" ");
+                }
+                break;
+            default:
+                for (int i = 0; i < sidefirst_2; i++) {
+                    for (int j = 0; j < sidesecond_2; j++) {
+                        if (i == 0 || j == 0 || i == sidefirst_2 - 1 || j == sidesecond_2 - 1)
+                            System.out.print(symbol_2);
+                        else
+                            System.out.print(" ");
+                    }
+                    System.out.println(" ");
+            }
+        }}
     public static void main(String[] args) {
+        //task 1
+        System.out.print("Enter your number: ");
+        int NuMbEr = number.nextInt();
+        int unit_1 = 0;
+        int unit_2 = 0;
+        int unit_3 = 0;
+        int unit_4 = 0;
+        int unit_5 = 0;
+        int sum = 0;
+        int avg = 0;
+
+        if (NuMbEr / 10000 > 0){
+            unit_1 = NuMbEr / 10000;
+            unit_2 = (NuMbEr % 10000) / 1000;
+            unit_3 = (NuMbEr % 1000) / 100;
+            unit_4 = (NuMbEr % 100) / 10;
+            unit_5 = NuMbEr % 10;
+
+            sum = unit_1 + unit_2 + unit_3 + unit_4 + unit_5;
+            avg = sum / 5;
+
+            //for (int i =0; i < 4; i++){
+            //    if(unit_i == 0)
+            //}
+            System.out.println("Your number consists of five number.");
+        }
+        else if(NuMbEr / 1000 > 0){
+            unit_1 = NuMbEr / 1000;
+            unit_2 = (NuMbEr % 1000) / 100;
+            unit_3 = (NuMbEr % 100) / 10;
+            unit_4 = NuMbEr % 10;
+            sum = unit_1 + unit_2 + unit_3 +unit_4;
+            avg = sum / 4;
+            System.out.println("Your number consists of four number.");
+        }
+        else if(NuMbEr / 100 > 0){
+            unit_1 = NuMbEr / 100;
+            unit_2 = (NuMbEr % 100) / 10;
+            unit_3 = NuMbEr % 10;
+            sum = unit_1 + unit_2 + unit_3;
+            avg = sum / 3;
+            System.out.println("Your number consists of thee number.");
+        }
+        else if(NuMbEr / 10 > 0){
+            unit_1 = NuMbEr / 10;
+            unit_2 = NuMbEr % 10;
+            sum = unit_1 + unit_2;
+            avg = sum / 2;
+            System.out.println("Your number consists of two number.");
+        }
+        else if(NuMbEr / 1 > 0){
+            unit_1 = NuMbEr;
+            System.out.println("Your number consists of one number.");
+        }
+        //i think about this.....
 
 
-    }
-}
 
+
+        //task 2
+        System.out.print("Enter height array: ");
+        int count = number.nextInt();
+        for (int i = 0; i < count / 2; i++) {
+            for (int j = 0; j < count; j++) {
+                System.out.print("***___");
+            }
+            System.out.println(" ");
+        }
+        for (int i = 0; i < count / 2; i++) {
+            for (int j = 0; j < count; j++) {
+                System.out.print("___***");
+            }
+            System.out.println(" ");
+        }
+
+
+
+    }}
